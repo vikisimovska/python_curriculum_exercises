901069b4c4bcd554214c83dd1580369798df30c7
Violeta Simovska <vikisimovska27@gmail.com>
2018-03-16 12:37:12 -0700

    Revert "merging"

--- hw/project/users/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, redirect, render_template, request, url_for
-from project.users.models import User
-from project import db
-# let's create the owners_blueprint to register in our __init__.py
-users_blueprint = Blueprint(
-    'users',
-    __name__,
-    template_folder='templates/users'
-)
